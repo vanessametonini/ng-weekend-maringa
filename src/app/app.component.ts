f3061e61d55c827bad2e697ab62ecff117e2173f
Vanessa Me Tonini <vanessametonini@gmail.com>
2019-02-02 17:06:44 -0200

    Workshop angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
+//decorator
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'planets';
+  title = 'ola mundo';
+  listaDePlanetas;
+
+  constructor(ajax: HttpClient){
+    ajax
+      .get('http://localhost:3000/v2/dados')
+      .subscribe(
+        (planetasApi) => {
+          this.listaDePlanetas = planetasApi
+        }
+      )
+  }
 }
---
+++ src/app/planeta/planeta.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'glx-planeta',
+  templateUrl: './planeta.component.html'
+})
+export class PlanetaComponent {
+  @Input() nomePlaneta = '';
+  @Input() url = ''
+           descricao = '';
+
+}
